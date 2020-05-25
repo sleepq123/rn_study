a391cdd20811682d116ed6ecb0e5662a1f135767
lichao <708941147@qq.com>
2020-05-25 14:49:31 +0800

    feat: 自定义NavigationBar

--- js/page/TreadingPage.js
+++ js/page/TreadingPage.js
@@ -1,24 +1,22 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
+
+import NavigationBar from '../components/NavigationBar';
+
 class TreadingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   render() {
     return (
-      <View style={styles.container}>
+      <View>
+        <NavigationBar title="趋势" leftButton={<Text>返回</Text>} />
         <Text>TreadingPage</Text>
       </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
+
 export default TreadingPage;
